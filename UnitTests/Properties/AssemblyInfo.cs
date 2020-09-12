19ab12c2df01a6f1bb37bcbaf019045559c2620e
Anderson Orjuela <anderg4@hotmail.com>
2020-09-12 14:14:11 -0500

    Agregar archivos de proyecto.

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0cfa008d-6991-4f7c-96e9-d333ab98ae55")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/Test/SumTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Test
+{
+    [TestClass]
+    public class SumTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            ///Patron AAA
+
+
+            ///Arange 
+
+            ApplicationCore.Services.Sum sum = new ApplicationCore.Services.Sum();
+            int num1 = 5;
+            int num2 = 2;
+
+            ///Act
+            var result = sum.SumValues(num1, num2);
+
+            ///Assert 
+            Assert.AreEqual(7, result);
+
+
+
+
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            ///Patron AAA
+
+
+            ///Arange 
+
+            ApplicationCore.Services.Sum sum = new ApplicationCore.Services.Sum();
+            int num1 = 5;
+            int num2 = 2;
+
+            ///Act
+            var result = sum.SumValues(num1, num2);
+
+            ///Assert 
+            Assert.AreEqual(7, result);
+        }
+    }
+}
